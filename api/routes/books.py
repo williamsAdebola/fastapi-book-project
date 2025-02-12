1d3f7742eeee1ca8032d7eaaa29d0013680abf2c
heiswillz98 <wheeliams98@gmail.com>
2025-02-12 11:59:07 +0100

    test

--- api/routes/books.py
+++ api/routes/books.py
@@ -48,6 +48,14 @@ async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
 
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int):
+    book = db.get_book(book_id)
+    if not book:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"detail": "Book not found"})
+    return book
+    
+
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def update_book(book_id: int, book: Book) -> Book:
     return JSONResponse(
